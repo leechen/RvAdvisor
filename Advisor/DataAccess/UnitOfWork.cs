88bfe1ef7a4493f3224828c3c8c05cd2a551f6df
leechen <leeh.chen@gmail.com>
2016-06-12 15:16:14 -0700

    Added DataAccess project

---
+++ Advisor/DataAccess/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Advisor.Sdk;
+
+namespace Advisor.DataAccess
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IQueryable<TEntity> Get<TEntity>() where TEntity : Entity
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
